2a3f2a0bcc889d268e0611d7992799f3a72da21b
skMobin <mobinshaikh180@gmail.com>
2019-12-25 02:47:39 +0530

    done with the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
 //check off the todos
-$("li").click(function(){  
+$("ul").on("click", "li",function(){  
     $(this).toggleClass("completed");
  })
-//
+//click on delete icon to delete particular to-do
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut("500",function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+//adding new to-do
+$("input[type='text']").on("keypress",function(event){
+    if(event.which == 13){
+        var toDoText = $(this).val();
+        $(this).val("");
+        $("ul").append('<li><span><i class="fa fa-trash"></i></span> '+ toDoText + "</li>");
+    }
+});
+//add input control
+$("pencil-icon").on("click",function(){
+    $("input[type='text']").fadeToggle("400");
+    if (document.getElementsByClassName("pencil-icon")[0].classList[2] === "fa-pencil"){
+        document.getElementsByClassName("pencil-icon")[0].classList[2] = "fa-minus";
+    }
+    else{
+        document.getElementsByClassName("pencil-icon")[0].classList[2] = "fa-pencil";
+    }
+});  
